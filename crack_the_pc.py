98ac23f2b729c49b2066d519ea74192c68162d24
Martin Burger <funnyfuntimes@icloud.com>
2025-04-24 19:46:06 -0400

    added a greating function

---
+++ crack_the_pc.py
@@ -0,0 +1,27 @@
+# martin burger
+# burger_m@lynchburg.edu
+# cs-131 final project
+# v0.1 of game 2.3: Cracking the Password
+
+import random
+
+def main():
+    greating()
+
+
+def greating():
+    print('\n\t'
+          'Welcome to ***Crack the Passcode!!!***\n\t'
+          'You will have 10 chances to crack the code.\n\t'
+          'The code length is 3 digits, and there will be no repeated numbers.\n\t'
+          'After submitting your answer, you will then receive a clue.\n\t'
+          'The clues are as follows.\n\t'
+          'A RED card means that all digits were incorrect.\n\t'
+          'A BLUE card means that one digit is correct but not in an incorrect position.\n\t'
+          'A GREEN card means that one digit is correct and is in the correct position.\n\t'
+          'Good LUCK!!!\n')
+
+
+
+## simply calling the main function
+main()
