8b46a5d90128f3888cab05571862a7334a255315
Martin Burger <funnyfuntimes@icloud.com>
2025-04-27 15:42:04 -0400

    improved functions

--- crack_the_pc.py
+++ crack_the_pc.py
@@ -8,44 +8,52 @@ import random
 
 def main():
     greating()
+    correct_string = str()
     correct_digits = list()
-    mk_passcode(correct_digits)
+    mk_passcode(correct_string, correct_digits)
+    print(f'{correct_digits} <- correct_digits@line.13.returned') #here for dev.testing
     user_digits = list()
-    user_ints = list()
-    get_guess(user_digits, user_ints)
+    user_string = str()
+    get_guess(user_digits, user_string)
+    print(f'{user_digits} <- user_digits@line16.returned') #here for dev.testing
     cards = list()
+    #user_ints = list()
     check_score(correct_digits, user_digits, cards)
 
 ## a simple welcome message
 ## that also outlines the rules
 def greating():
     print('\n\t'
-          'Welcome to ***Crack the Passcode!!!***\n\t'
-          'You will have 10 chances to crack the code.\n\t'
-          'The code length is 3 digits, and there will be no repeated numbers.\n\t'
-          'After submitting your answer, you will then receive a clue.\n\t'
-          'The clues are as follows.\n\t'
-          'A RED card means that all digits were incorrect.\n\t'
-          'A BLUE card means that one digit is correct but not in an incorrect position.\n\t'
-          'A GREEN card means that one digit is correct and is in the correct position.\n\t'
-          'Good LUCK!!!\n')
+          '*** Welcome to Crack the Passcode!!! ***\n\t'
+          ' You will have 10 chances to crack the code.\n\t'
+          ' The code length is 3 digits, and there will be no repeated numbers.\n\t'
+          ' After submitting your answer, you will then receive a clue.\n\t'
+          ' The clues are as follows.\n\t'
+          ' A RED card means that all digits were incorrect.\n\t'
+          ' A BLUE card means that one digit is correct but not in an incorrect position.\n\t'
+          ' A GREEN card means that one digit is correct and is in the correct position.\n\t'
+          ' Good LUCK!!!\n')
 
 ## time to generate a new passcode to be cracked
-def mk_passcode(correct_digits):
+def mk_passcode(correct_string, correct_digits):
     number_pool = (range(0,10))
-    correct_digits = (random.sample(number_pool, 3)) 
+    correct_string = (random.sample(number_pool, 3)) 
+    for digit in correct_string:
+        correct_digits.append(int(digit))
+    print(f'{correct_digits} <-correct_digits@mk_passcode()')
+    
     return correct_digits
 
-def get_guess(user_digits, user_ints):
+def get_guess(user_digits, user_string): #, user_ints):
     print('What do you think the passcode is?')
-    user_digits = int(input('> '))
+    user_string = (input('> '))
+    for digit in user_string:
+        user_digits.append(int(digit))
+
     return user_digits
 
-def convert_user_data_type(user_digits, user_int):
-    for digit in user_digits:
-        user_ints.append(int(digit))
-
 def check_score(correct_digits, user_digits, cards):
+    print(user_digits)
     if user_digits[0] == correct_digits[0]:
         cards.append('GREEN Card')
         if user_digits[1] == correct_digits[1]:
@@ -71,9 +79,13 @@ def check_score(correct_digits, user_digits, cards):
     else:
         blue_or_red(user_digits, correct_digits, cards)
 
+
+# needs work -> this will generate a blue even if
+# (digit) already got a GREEN Card
+# I need to find a way to filter GREEN Card winning digits out
 def blue_or_red(user_digits, correct_digits, cards):
     for digit in user_digits:
-        if digit in correct_digits: # and .index('BLUE Card') != cards.index():
+        if digit in correct_digits:
             cards.append('BLUE Card')
         elif digit not in correct_digits:
             cards.append('RED Card')
